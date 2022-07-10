941731f0a387f1f6221dd6492b1bcaf3a2a4fcb4
Carmen <carmen020824@gmail.com>
2022-07-10 12:07:25 +0800

    create post request

--- Website/script.js
+++ Website/script.js
@@ -1,9 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic21vaGFuMjAwMSIsImEiOiJjbDVkaDZiNmwwNmwwM2ZvMXYwbDVieG9uIn0.hpEwFPqANepe3lUep9EU1Q';
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/smohan2001/cl5dh88vv000o15pi194xhvy2', // style URL
-    center: [115.816,-31.980], // starting position [lng, lat]
-    zoom: 13 // starting zoom
+  container: 'map', // container ID
+  style: 'mapbox://styles/smohan2001/cl5dh88vv000o15pi194xhvy2', // style URL
+  center: [115.816, -31.980], // starting position [lng, lat]
+  zoom: 13 // starting zoom
 });
 
 map.on('style.load', () => {
@@ -13,7 +13,7 @@ map.on('style.load', () => {
 map.addControl(
   new mapboxgl.GeolocateControl({
     positionOptions: {
-    enableHighAccuracy: true
+      enableHighAccuracy: true
     },
     trackUserLocation: true,
     showUserHeading: true,
@@ -24,7 +24,8 @@ map.addControl(
 const direction = new MapboxDirections({
   accessToken: mapboxgl.accessToken,
   unit: 'metric',
-  profile: 'mapbox/driving'
+  profile: 'mapbox/driving',
+  coordinates: '{115.80513},{-31.979904};{115.826594},{-31.952633};{115.877596},{-31.963431}'
 })
 
 direction.on('route', (event) => {
@@ -35,14 +36,35 @@ direction.on('route', (event) => {
   for (let i = 0; i < stepsLen; i++) {
     intersectionLen = allSteps[i].intersections.length
     intersections = allSteps[i].intersections
-    for (let j = 0; j < intersectionLen; j++){
-      pointsArr.push([intersections[j].location[0],intersections[j].location[1]])
+    for (let j = 0; j < intersectionLen; j++) {
+      pointsArr.push([intersections[j].location[1], intersections[j].location[0]])
     }
   }
   // Need to send pointsArr to backend
   console.log(pointsArr)
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "/servo", true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    path: pointsArr,
+    efficiency: 5,
+    capacity: 50,
+    current_tank: 2,
+    RAC: 0,
+    Woolies: 0
+  }));
+  xhr.onload = function () {
+    console.log("HELLO")
+    console.log(this.responseText);
+    var data = JSON.parse(this.responseText);
+    console.log(data);
+  }
 });
 
 map.addControl(
   direction
 );
+
+func sendData(){
+
+}
